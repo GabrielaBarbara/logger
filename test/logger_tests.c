83c6093dc6f22e73829e24b02ff0fe2d8d562751
Gabriela Gibson <gbg@apache.org>
2015-09-20 05:36:54 +0100

    Added custom debug level tests

--- test/logger_tests.c
+++ test/logger_tests.c
@@ -30,6 +30,13 @@ const int LOG_FIRST_CUSTOM_LOG_LEVEL = LOG_BASE_COUNT + __COUNTER__;
 const int LOG_SECOND_CUSTOM_LOG_LEVEL = LOG_BASE_COUNT + __COUNTER__;
 #define LOG_SECOND_CUSTOM_MSG(msg,...) DEFINE_LOG_MSG("SECOND_CUSTOM_LOG_LEVEL",LOG_SECOND_CUSTOM_LOG_LEVEL, msg, __VA_ARGS__);
 
+/* Some custom debug levels */
+const int LOG_debug_loop = LOG_BASE_COUNT + __COUNTER__;
+#define LOG_debug_loop_msg(msg,...) DEFINE_LOG_MSG("LOG_debug_loop",LOG_debug_loop, msg, __VA_ARGS__);
+
+const int LOG_debug_while = LOG_BASE_COUNT + __COUNTER__;
+#define LOG_debug_while_msg(msg,...) DEFINE_LOG_MSG("LOG_debug_while",LOG_debug_while, msg, __VA_ARGS__);
+
 /* Set a custom output function */
 void custom_output_function(char *prefix, char* contents)
 {
@@ -62,34 +69,50 @@ void logger_test(char *show_what)
 
 int main(int argc, char *argv[])
 {
+    /* Set output to a custom function defined in this file */
     log_set_output_function(custom_output_function);
 
+    /*******************************************************/
+    //    Single level tests -- nothing, upto, exact
+    /*******************************************************/
     log_set_level(SHOW_NOTHING, LOG_ERR);
     logger_test("Show nothing");
 
+    /*******************************************************/
     log_set_level(SHOW_LOG_LEVEL_INCLUDING, LOG_INFO);
-    logger_test("show all log levels upto including LOG_INFO");
+    logger_test("Show all log levels upto including LOG_INFO");
 
+    /*******************************************************/
     log_set_level(SHOW_EXACT_LOG_LEVEL, LOG_NOTICE);
-    logger_test("show only log level LOG_NOTICE");
+    logger_test("Show only log level LOG_NOTICE");
 
+    /*******************************************************/
+    // Selective levels test -- several and single
+    /*******************************************************/
     int selection[] = {LOG_TODO, LOG_WARN, LOG_FIRST_CUSTOM_LOG_LEVEL};
     log_set_level_selection(selection, 3);
 
     log_set_level(SHOW_SELECT_LOG_LEVELS, LOG_FIRST_CUSTOM_LOG_LEVEL);
     logger_test("Show selected log levels up to LOG_FIRST_CUSTOM_LOG_LEVEL");
 
+    /*******************************************************/
     log_set_level(SHOW_EXACT_LOG_LEVEL, LOG_SECOND_CUSTOM_LOG_LEVEL);
     logger_test("Show just your second custom log message");
 
+    /*******************************************************/
+    // Unix log file output tests
+    /*******************************************************/
     log_set_output_function(log_print_to_file);
     log_file_init("/home/g/logger/logs/", 
                   "/home/g/logger/build/",  
                   NO_HOSTNAME,
                   LOG_APPEND);
 
+
     log_set_level(SHOW_SELECT_LOG_LEVELS, LOG_SECOND_CUSTOM_LOG_LEVEL);
     logger_test("Write to log file:  selected log levels up to LOG_SECOND_CUSTOM_LOG_LEVEL");
+
+    /*******************************************************/
     log_set_level(SHOW_EXACT_LOG_LEVEL, LOG_NOTICE);
     logger_test("Write to log file:  exact log level -> LOG_NOTICE");
     log_file_init("/home/g/logger/logs/", 
@@ -99,9 +122,8 @@ int main(int argc, char *argv[])
 
     log_set_level(SHOW_SELECT_LOG_LEVELS, LOG_SECOND_CUSTOM_LOG_LEVEL);
     logger_test("Write to log file:  selected log levels up to LOG_SECOND_CUSTOM_LOG_LEVEL");
-    log_set_level(SHOW_EXACT_LOG_LEVEL, LOG_NOTICE);
-    logger_test("Write to log file:  exact log level -> LOG_NOTICE");
 
+    /*******************************************************/
     log_file_init("/home/g/logger/logs/", 
                   "/home/g/logger/build/",  
                   WITH_HOSTNAME,
@@ -110,5 +132,27 @@ int main(int argc, char *argv[])
     logger_test("Write to log file:  exact log level -> LOG_ERR");
 
     close_log();
+
+    /*******************************************************/
+    // TODO and custum debug levels
+    /*******************************************************/
+
+    log_set_output_function(custom_output_function);    
+    int second_selection[] = {LOG_TODO, LOG_debug_while, LOG_debug_loop};
+    log_set_level_selection(second_selection, 3);
+    log_set_level(SHOW_SELECT_LOG_LEVELS, LOG_debug_while);
+    LOG_TODO_MSG("You should get some coffee...","");
+
+    int i = 0;
+    for (; i < 5; i++) {
+        if (3 == i)
+            LOG_debug_loop_msg("The value of i = %d",i);
+    }
+    
+    while (--i)
+        if (3 == i)
+            LOG_debug_while_msg("You roll a %d-sided die %d times.  WINNER!!!",57839, 75899032);
+
+
     return 0;
 }
